95e000074e1d1a4032ed5db80e66945eb3148baf
kaywon <kaywon@umich.edu>
2020-10-26 18:50:16 -0400

    data.sql and schema.sql are populated, added a .gitignore file, and made changed to politiquizdb script

---
+++ sql/data.sql
@@ -0,0 +1,106 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('1', 'The United States should increase military spending.','Foreign Policy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('2', 'The United States should accept more refugees.','Foreign Policy','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('3', 'The United States should remain in NATO.','Foreign Policy','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('4', 'The United States should continue NSA surveillance of its allies.','Foreign Policy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('5', 'The United States should increase funding for nuclear weapons.','Foreign Policy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('6', 'The rich are too highly taxed in the United States.','Economy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('7', 'The government should raise the minimum wage to $15.','Economy','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('8', 'The govnment must reduce public spending to reduce national debt.','Economy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('9', 'Economic stimulus should be used to aid the country during a recession.','Economy','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('10', 'The United States should increase tarrifs on products imported from China.','Economy','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('11', 'I am in favor of building a wall along the US-Mexico border.','Immigration','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('12', 'Children of illegal immigrants should be granted US citizenship.','Immigration','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('13', 'The United States should increase the number of temporary work visas given to highly skilled immigrants.','Immigration','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('14', 'Immigrants should be required to pass a citizenship test to demonstrate an understanding of US language, history and government.','Immigration','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('15', 'Immigrants should be deported if they commit a serious crime.','Immigration','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('16', 'There should be more restrictions on the current process of purchasing a firearm.','Domestic','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('17', 'I am in support of the Patriot Act.','Domestic','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('18', 'I am in support of reducing funding for police, and redirecting funding to social and community programs.','Domestic','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('19', 'Marijuana should be legalized by the federal government.','Domestic','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('20', 'The government should hire private companies to run prisons.','Domestic','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('21', 'The government should introduce a corporate carbon tax to combat climate change.','Environment','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('22', 'I support fracking for extracting oil and natural gas resources.','Environment','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('23', 'Researchers should be allowed to user animals for testing safety of drugs and vaccines.','Environment','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('24', 'The United States should withdraw from the Paris Climate Agreement.','Environment','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('25', 'I am in favor of the Green New Deal.','Environment','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('26', 'I am in favor of the Patient Protection and Affordable Care Act (Obamacare).','Healthcare','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('27', 'The government should reduce funding to the World Health Organization.','Healthcare','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('28', 'Individuals should have the choice to use private healthcare options.','Healthcare','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('29', 'The government should regulate prices of live-saving drugs.','Healthcare','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('30', 'The government should mandate masks during the COVID-19 pandemic.','Healthcare','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('31', 'The federal government should fund undergraduate tuition at public colleges at university.','Education','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('32', 'Charter schools are an effective system to combat educational inequity.','Education','Conservative');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('33', 'School attendance should not be made compulsory.','Education','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('34', 'I am in favor of the Common Core national education standards.','Education','Liberal');
+
+INSERT INTO questions(qid, text, topic, lean)
+VALUES ('35', 'It is important for educational institutions to instill religious values.','Education','Conservative');
---
+++ sql/schema.sql
@@ -0,0 +1,18 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE questions (
+	qid INTEGER NOT NULL,
+	text VARCHAR(1024) NOT NULL,
+	topic VARCHAR(20) NOT NULL,
+	lean INTEGER NOT NULL,
+  	PRIMARY KEY(qid)
+);
+
+CREATE TABLE answers (
+    aid INTEGER NOT NULL,
+    qid INTEGER NOT NULL,
+	text INTEGER NOT NULL,
+	lean INTEGER NOT NULL,
+	PRIMARY KEY (qid, aid),
+	FOREIGN KEY (qid) REFERENCES questions(qid) ON UPDATE CASCADE ON DELETE CASCADE
+);
