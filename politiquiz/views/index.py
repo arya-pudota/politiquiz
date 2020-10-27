c00dcd4c129716f6d8ce6370baae371c75af0d33
Nilay <Nilay.Thummar@jazeeraairways.com>
2020-10-27 19:50:35 -0400

    Moved rendering quiz page from index.py to quiz.py

--- politiquiz/views/index.py
+++ politiquiz/views/index.py
@@ -7,28 +7,4 @@ import politiquiz
 
 @politiquiz.app.route("/", methods=["GET", "POST"])
 def show_index():
-    """
-    context : {
-        "questions": [
-            {
-                "question_id": question_id
-                "question_text" : question_text
-                "question_topic": question_topic
-            },
-            ....
-        ]
-    }
-    """
-    connection = politiquiz.model.get_db()
-
-    questions = connection.execute("SELECT qid, text, topic FROM questions").\
-        fetchall()
-    context = {"questions": []}
-    for question in questions:
-        context["questions"].append({
-            "question_id": question.qid,
-            "question_text": question.text,
-            "question_topic": question.topic,
-            })
-
-    return flask.render_template("index.html", **context)
+    pass
--- politiquiz/views/quiz.py
+++ politiquiz/views/quiz.py
@@ -1,11 +1,33 @@
 """Back-end flask routing for quiz.html page."""
 
 import flask
-import arrow
-
 import politiquiz
-from .helpers import get_formatted_comments, add_comment, add_like, remove_like
 
 
 @politiquiz.app.route("/p/<postid>/", methods=["GET", "POST"])
 def show_quiz(postid):
+    """
+        context : {
+            "questions": [
+                {
+                    "question_id": question_id
+                    "question_text" : question_text
+                    "question_topic": question_topic
+                },
+                ....
+            ]
+        }
+        """
+    connection = politiquiz.model.get_db()
+
+    questions = connection.execute("SELECT qid, text, topic FROM questions"). \
+        fetchall()
+    context = {"questions": []}
+    for question in questions:
+        context["questions"].append({
+            "question_id": question.qid,
+            "question_text": question.text,
+            "question_topic": question.topic,
+            })
+
+    return flask.render_template("index.html", **context)
