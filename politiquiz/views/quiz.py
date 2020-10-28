de93a100b0e34eb7d59bc5028d3d69205f27e1ce
Nilay <Nilay.Thummar@jazeeraairways.com>
2020-10-27 20:05:10 -0400

    Changed structure of context for quiz page

--- politiquiz/views/quiz.py
+++ politiquiz/views/quiz.py
@@ -4,30 +4,33 @@ import flask
 import politiquiz
 
 
-@politiquiz.app.route("/p/<postid>/", methods=["GET", "POST"])
-def show_quiz(postid):
+@politiquiz.app.route("/quiz/", methods=["GET", "POST"])
+def show_quiz():
     """
         context : {
-            "questions": [
+            "question_topics":
+            {
+                "question_topic": [
                 {
                     "question_id": question_id
                     "question_text" : question_text
-                    "question_topic": question_topic
                 },
                 ....
-            ]
+                ]
+            }
         }
-        """
+    """
     connection = politiquiz.model.get_db()
 
     questions = connection.execute("SELECT qid, text, topic FROM questions"). \
         fetchall()
-    context = {"questions": []}
+    context = {"question_topics": {}}
     for question in questions:
-        context["questions"].append({
+        if not context["question_topics"][question["question_topic"]]:
+            context["question_topics"][question["question_topic"]] = []
+
+        context["question_topics"][question["question_topic"]].append({
             "question_id": question.qid,
             "question_text": question.text,
-            "question_topic": question.topic,
             })
-
-    return flask.render_template("index.html", **context)
+    return flask.render_template("quiz.html", **context)
