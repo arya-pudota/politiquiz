78fa0ab0bfaa3d1c0ca8bbd563498776f2d5d8b2
Arya Pudota <pudota@umich.edu>
2020-10-27 20:51:23 -0400

    completed the GET and POST requests for the quiz page

--- politiquiz/views/quiz.py
+++ politiquiz/views/quiz.py
@@ -21,16 +21,39 @@ def show_quiz():
         }
     """
     connection = politiquiz.model.get_db()
+    questions = connection.execute("SELECT qid, text, topic, lean FROM questions"). \
+        fetchall()
+
+    if flask.request.method == "POST":
+        political_score = 0
+        question_answers = dict(flask.request.form)
+        print(question_answers)
+        print(questions)
+        num_answers = len(question_answers)
+        for answer in question_answers:
+            for question in questions:
+                if int(answer) == question["qid"]:
+                    if question["lean"] == "Conservative":
+                        political_score += int(question_answers[answer])
+                    elif question["lean"] == "Liberal":
+                        political_score += 6 - int(question_answers[answer])
+                    break
+        political_score = political_score / num_answers
+        if political_score > 3:
+            flask.redirect(flask.url_for('show_resources', type="Republican"))
+        else:
+            flask.redirect(flask.url_for('show_resources', type="Democratic"))
+
 
     questions = connection.execute("SELECT qid, text, topic FROM questions"). \
         fetchall()
     context = {"question_topics": {}}
     for question in questions:
-        if not context["question_topics"][question["question_topic"]]:
-            context["question_topics"][question["question_topic"]] = []
+        if question["topic"] not in context["question_topics"]:
+            context["question_topics"][question["topic"]] = []
 
-        context["question_topics"][question["question_topic"]].append({
-            "question_id": question.qid,
-            "question_text": question.text,
+        context["question_topics"][question["topic"]].append({
+            "question_id": question["qid"],
+            "question_text": question["text"],
             })
     return flask.render_template("quiz.html", **context)
--- politiquiz/views/resources.py
+++ politiquiz/views/resources.py
@@ -2,8 +2,8 @@
 
 import flask
 import politiquiz
-from .helpers import save_file
 
 
 @politiquiz.app.route("/u/<username>/", methods=["GET", "POST"])
 def show_resources(username):
+    pass
