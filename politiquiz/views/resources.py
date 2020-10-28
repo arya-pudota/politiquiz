b4ecdc75b12a15ff22f7f9565416f5ee94a02ea3
Sanjit Rajendiran <sanjitr@comcast.net>
2020-10-28 12:17:46 -0700

    Update resources.py

--- politiquiz/views/resources.py
+++ politiquiz/views/resources.py
@@ -4,6 +4,10 @@ import flask
 import politiquiz
 
 
-@politiquiz.app.route("/resources/", methods=["GET", "POST"])
-def show_resources(username):
-    return flask.render_template("resources.html")
+@politiquiz.app.route("/resources/<type>/", methods=["GET"])
+def show_resources(type):
+    if type == 'republican':
+        return flask.render_template("republican.html")
+
+    if type == 'democratic':
+        return flask.render_template("democratic.html")
